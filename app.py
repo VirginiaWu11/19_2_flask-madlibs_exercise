fd6af09884facf731faae5e288f2e51418fbbe63
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-04-18 11:57:52 -0400

    complete

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask, request, render_template
+from random import randint,  choice, sample
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "m12345"
+debug = DebugToolbarExtension(app)
+
+
+
+
 """Madlibs Stories."""
 
 
@@ -40,6 +52,17 @@ class Story:
 
 story = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+    """Once upon a time in a long-ago {place}, there lived a large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
+
+
+@app.route('/')
+def home():
+    return render_template('madlibs.html', prompts = story.prompts)
+
+@app.route('/story')
+def madlibs_story():
+    result = story.generate(request.args)
+    return render_template('story.html',result = result)
+    
+
